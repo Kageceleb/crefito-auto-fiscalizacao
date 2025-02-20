f55831c2c1740017ac453a680e35002e6acb1813
Samuel Sant Anna <samuelsantaanna@gmail.com>
2025-02-20 10:22:38 -0300

    create basic layout with banner

---
+++ www/src/components/form/index.tsx
@@ -0,0 +1,9 @@
+import './styles.css'
+
+export function Form() {
+    return (
+        <div className='form'>
+            here goes the form
+        </div>
+    )
+}
---
+++ www/src/components/header/index.tsx
@@ -0,0 +1,10 @@
+import './styles.css'
+import banner from "../../assets/banner.jpg"
+
+export function Header() {
+    return (
+        <div className="header">
+            <img src={banner} className="banner" />
+        </div>
+    )
+}
