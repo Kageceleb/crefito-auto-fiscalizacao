b04b0b5480f5a86f4dd5229f93ae70aa0d8ae028
Samuel Sant Anna <samuelsantaanna@gmail.com>
2025-03-09 17:32:04 -0300

    add feedback

--- www/answers.ts
+++ www/answers.ts
@@ -1,25 +1,26 @@
 import { questionarieAnswer } from "./types/answerType";
 
-export const mockAnswer: questionarieAnswer[]  = [
+export const mockAnswer: questionarieAnswer[] = [
 
   {
     "id": 1,
-    "answer": "",
+    "answer": "Apure se o hospital possui registro junto:\na) ao CREFITO-5; e\nb) à Vigilância Sanitária.\n\nPara verificar se o local possui registro no CREFITO-5, acesse a página da “Consulta de Inscritos” do CREFITO-5 < https://crefito-rs.implanta.net.br/servicosonline/Publico/ConsultaInscritos/ > e pesquise se o hospital em questão possui registro ativo e regular junto ao Conselho. Se encontrar dificuldades, entre em contato com a nossa equipe de atendimento através do Whatsapp: (51) 3334-6586 ou por e-mail: atendimento@crefito5.org.br.\n\nLembramos que, é obrigatório o registro das entidades que prestam serviços de fisioterapia e terapia ocupacional junto ao CREFITO, conforme dispõe o parágrafo único do art. 12 da Lei 6.316/1975 e Lei 6.839/1980, bem como, é indispensável o registro junto à Vigilância Sanitária, nos termos previstos no art. 8º da Resolução ANVISA RDC nº 7/2010.",
+
     "shouldAnswer": (answers) => { return answers[1] === "Não" || answers[1] === "Não Sei" },
   },
   {
     "id": 2,
-    "answer": "",
+    "answer": "reposta 2",
     "shouldAnswer": (answers) => { return answers[2] === "Não" || answers[3] === "Não" || answers[4] === "Não" },
   },
   {
     "id": 3,
-    "answer": "",
+    "answer": "reposta 3",
     "shouldAnswer": (answers) => { return answers[5] === "Não" },
   },
   {
     "id": 6,
-    "answer": "",
+    "answer": "reposta 4",
     "shouldAnswer": (answers) => { return answers[10] === "Não" },
   },
 ]
--- www/src/components/questionsForm/index.tsx
+++ www/src/components/questionsForm/index.tsx
@@ -8,14 +8,23 @@ type AnswerValues = { [key: number]: string }
 
 export const QuestionForm: React.FC<{ questions: question[], answers: questionarieAnswer[] }> = ({ questions, answers }) => {
     const [presentQuestionIndex, setPresentQuestionIndex] = useState(-1);
+    const [feedback, setFeedback] = useState<questionarieAnswer[]>([]);
     const presentQuestion = questions[presentQuestionIndex];
     const nextIndex = presentQuestionIndex + 1;
     const nextQuestion = (values: AnswerValues) => {
         console.log(values);
-        const nextQuestion = questions[nextIndex] ;
-        if (!nextQuestion) setPresentQuestionIndex(presentQuestionIndex +1);
-        setPresentQuestionIndex((nextQuestion.shouldSkip?.(values)) ? nextIndex + 1 : nextIndex)
+        const nextQuestion = questions[nextIndex];
+        if (!nextQuestion) {
+            console.log(answers, values)
+
+            const feedbacks = answers.filter((answer) => {
+                return (answer.shouldAnswer?.(values))
+            })
+            setFeedback(feedbacks)
+        }
+        setPresentQuestionIndex((nextQuestion?.shouldSkip?.(values)) ? nextIndex + 1 : nextIndex)
     }
+
     return (
         presentQuestionIndex === -1 ?
             (<div className='form'>
@@ -23,44 +32,49 @@ export const QuestionForm: React.FC<{ questions: question[], answers: questionar
                 <button className='nextQuestion' type='button' onClick={() => setPresentQuestionIndex(0)}> Começar </button>
             </div>)
             : nextIndex > questions.length ?
-            (<h1>{answers[0].id}</h1>)
-            :
-            (<div className='form'>
-                <Formik
-                    initialValues={{}}
-                    onSubmit={(values) => {
-                        console.log(values);
-                    }}
-                >
-                    {({ values }) => (
-                        <Form >
-                            <div key={presentQuestion.id}>
-                                <div className='question'>
-                                    <label className='number'> Pergunta {presentQuestion.id}</label>
-                                    <label >{presentQuestion.question}</label>
+                (<div className='feedback'>
+                    {feedback.map((item) => {
+                        return <h1>{item.answer}</h1>
+                    })}
+                </div>
+                )
+                :
+                (<div className='form'>
+                    <Formik
+                        initialValues={{}}
+                        onSubmit={(values) => {
+                            console.log(values);
+                        }}
+                    >
+                        {({ values }) => (
+                            <Form >
+                                <div key={presentQuestion.id}>
+                                    <div className='question'>
+                                        <label className='number'> Pergunta {presentQuestion.id}</label>
+                                        <label >{presentQuestion.question}</label>
+                                    </div>
+                                    <div className='answers'>
+                                        {presentQuestion.type === "radio" ? (
+                                            presentQuestion.options.map((option) => (
+                                                <label className='answer' key={option}>
+                                                    <Field type="radio" name={presentQuestion.id} value={option} />
+                                                    {option}
+
+                                                </label>
+                                            ))
+                                        ) : (
+                                            <h1>not radio</h1>
+                                        )
+                                        }
+                                    </div>
+
                                 </div>
-                                <div className='answers'>
-                                    {presentQuestion.type === "radio" ? (
-                                        presentQuestion.options.map((option) => (
-                                            <label className='answer' key={option}>
-                                                <Field type="radio" name={presentQuestion.id} value={option} />
-                                                {option}
+                                <button className='nextQuestion' type='button' onClick={() => nextQuestion(values)}> Próxima pergunta</button>
 
-                                            </label>
-                                        ))
-                                    ) : (
-                                        <h1>not radio</h1>
-                                    )
-                                    }
-                                </div>
+                            </Form>
+                        )}
 
-                            </div>
-                            <button className='nextQuestion' type='button' onClick={() => nextQuestion(values)}> Próxima pergunta</button>
-
-                        </Form>
-                    )}
-
-                </Formik>
-            </div >)
+                    </Formik>
+                </div >)
     );
 }
