5310c3b49585fccedd11c8911bdf902652348702
Samuel Sant Anna <samuelsantaanna@gmail.com>
2025-03-13 21:42:22 -0300

    add component PresentationText

---
+++ docs/src/components/presentationText/index.tsx
@@ -0,0 +1,18 @@
+export const PresentationText: React.FC = () => {
+    return (
+        <div>
+            <h1>  Autofiscalização: <p> a ferramenta para garantir a regularidade da sua atuação!</p></h1>
+
+            <p>
+                Se você é responsável técnico, sabe da importância de manter sua atuação e a da sua equipe dentro das normas. Com o Autofiscalizatório, você pode avaliar rapidamente se há alguma irregularidade e alertar seu gestor antes que problemas surjam.
+            </p>
+
+            <p>
+                Responda algumas perguntas e tenha um diagnóstico claro sobre a conformidade profissional. Identifique pontos de atenção e tome providências para garantir um ambiente regular e seguro.
+            </p>
+            <p>
+                Não espere uma fiscalização para descobrir problemas! Use o Autofiscalizatório e mantenha sua responsabilidade técnica sempre em dia com o Conselho!
+            </p>
+        </div>
+    )
+}
